377ed26ffc5d69e52e2008d5ed5f63d43bbbd666
bleemesser <54053294+bleemesser@users.noreply.github.com>
2025-10-11 22:25:46 -0400

    cleanup

--- shaders/julia_set.fs
+++ shaders/julia_set.fs
@@ -1,13 +1,10 @@
 #version 330
 
-// Input vertex attributes (from vertex shader)
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
-// Output fragment color
 out vec4 finalColor;
 
-// Uniforms from the application
 uniform vec2 c;
 uniform vec2 offset;
 uniform float zoom;
@@ -16,7 +13,6 @@ uniform vec2 screenSize;
 const int maxIterations = 255;
 const float colorCycles = 2.0;
 
-// Square a complex number
 vec2 ComplexSquare(vec2 z) {
     return vec2(
         z.x * z.x - z.y * z.y,
@@ -24,7 +20,6 @@ vec2 ComplexSquare(vec2 z) {
     );
 }
 
-// Convert Hue Saturation Value (HSV) color into RGB
 vec3 Hsv2rgb(vec3 c) {
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
@@ -32,18 +27,13 @@ vec3 Hsv2rgb(vec3 c) {
 }
 
 void main() {
-    // --- Aspect Ratio Correction ---
-    // Convert fragment coordinates from [0, 1] to [-0.5, 0.5]
     vec2 uv = fragTexCoord - 0.5;
-    // Adjust the x-coordinate based on the screen's aspect ratio
     uv.x *= screenSize.x / screenSize.y;
 
-    // Scale and position the view
     vec2 z = uv * 2.5 / zoom;
     z.x += offset.x;
-    z.y -= offset.y; // Invert Y-axis for correct panning
+    z.y -= offset.y;
 
-    // --- Julia Set Iteration ---
     int iterations = 0;
     for (int i = 0; i < maxIterations; i++) {
         z = ComplexSquare(z) + c;
@@ -53,19 +43,12 @@ void main() {
         iterations++;
     }
 
-    // --- Color Smoothing ---
-    // Perform a few extra iterations to get a more accurate value for smoothing
-    // See: http://linas.org/art-gallery/escape/escape.html
     z = ComplexSquare(z) + c;
     z = ComplexSquare(z) + c;
     float smoothVal = float(iterations) + 1.0 - (log(log(length(z))) / log(2.0));
 
-    // Normalize the value to be between 0 and 1
     float norm = smoothVal / float(maxIterations);
 
-    // --- Final Color Calculation ---
-    // If the point is in the set (doesn't escape), color it black.
-    // Otherwise, map the normalized value to a color from the HSV palette.
     if (iterations == maxIterations) {
         finalColor = vec4(0.0, 0.0, 0.0, 1.0);
     } else {
